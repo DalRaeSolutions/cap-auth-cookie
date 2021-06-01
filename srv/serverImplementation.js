3ebccd4555cfb5c105ef9c8ee38a2ea39a9a2431
jorgt <jorg.thuijls@gmail.com>
2021-06-02 08:50:54 +1000

    secure cookie

--- srv/serverImplementation.js
+++ srv/serverImplementation.js
@@ -1,13 +1,11 @@
 const passport = require('passport');
 const auth = require('../auth/authMockStrategy');
 const cookie = require('cookie-parser');
-const cookieOptions = { expires: new Date(Date.now() + 900000), httpOnly: true, secure: true }
+const cookieOptions = { expires: new Date(Date.now() + 900000), httpOnly: true, secure: true, signed: true }
 
 module.exports = async (app) => {
   await passport.use(new auth());
-  await app.use(cookie({
-    secret: 'brs-cookie-secret'
-  }));
+  await app.use(cookie('brs-cookie-secret'));
   await app.use(passport.initialize());
   await app.use(passport.authenticate('mock', { session: false }))
 
@@ -21,8 +19,7 @@ module.exports = async (app) => {
   });
 
   app.use(async (req, res, next) => {
-    console.log(req.cookies);
-    console.log(req.signedCookies);
+    console.log(req?.signedCookies?.userattributes);
 
     next();
   });
